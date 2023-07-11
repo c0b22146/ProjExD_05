ff8e0bf88db3b1258795eca860a712660e70051a
c0b22146 <c0b22146af@edu.teu.ac.jp>
2023-07-11 17:22:48 +0900

    追加機能:Enemy

--- base.py
+++ base.py
@@ -9,6 +9,7 @@ import sys
 import pygame as pg
 import random
 import numpy as np
+from pygame.sprite import AbstractGroup
 
 
 # class objects
@@ -77,6 +78,48 @@ class Character:
         """"""
         screen.blit(self.image, self.rect)
         return
+    
+class Enemy(pg.sprite.Sprite):
+    """
+        class about Enemy
+    """
+    enemy_size = np.array((30, 30))
+    enemy_down_speed = np.array((0, 7))
+    
+    def __init__(self):
+        """
+            敵Surfaceを生成する
+        """
+        super().__init__()
+        self.image = pg.Surface(self.enemy_size)
+        pg.draw.rect(self.image, (255, 0, 0), (0, 0, *self.enemy_size))
+        self.rect = self.image.get_rect()
+        self.rect.center = random.randint(200,980), 0
+        self.enemy_speed = 0
+        while -3 <= self.enemy_speed and self.enemy_speed <= 3:
+            self.enemy_speed = random.randint(-7,7)
+        return
+    
+    def update(self, fields: list) -> None:
+        """
+            敵の位置を更新する
+        """
+        self.rect[:-2] = np.array(self.rect[:-2]) + self.enemy_down_speed
+        for field in fields:
+            if field.get_rect().colliderect(self.rect):
+                self.rect[:-2] = np.array(self.rect[:-2]) - self.enemy_down_speed
+                self.rect.move_ip(+self.enemy_speed, 0)
+        if self.rect.left < 0 or 1000 < self.rect.right:
+            self.enemy_speed *= -1
+        return
+    
+    
+    def draw(self, screen: pg.Surface) -> None:
+        """
+            敵の描画
+        """
+        screen.blit(self.image, self.rect)
+        return
 
 
 def end(clear: bool) -> None:
@@ -96,6 +139,9 @@ def main(screen: pg.Surface) -> bool | None:
     # setup Surface
     fields: list[Field] = [Field()]
     chara = Character()
+    enemy_num = 4
+    enemys = pg.sprite.Group()
+    enemys.add(Enemy() for _ in range(enemy_num))
 
     # main loop process
     done = False
@@ -121,14 +167,20 @@ def main(screen: pg.Surface) -> bool | None:
         if key_pressed[pg.K_RIGHT]:
             chara.move("R")
 
+        if len(pg.sprite.spritecollide(chara, enemys, True)) != 0:
+            end(False)
+
+
         # update
         chara.update(fields)
+        enemys.update(fields)
 
         # draw
         screen.fill("white", (0, 0, 1000, 600))
         for field in fields:
             field.draw(screen)
         chara.draw(screen)
+        enemys.draw(screen)
         pg.display.update()
 
         # tike process
